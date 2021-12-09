7ca5b89039615b0ca3340bb50ddf3b641b86095d
Jona <retratos.jonatan@gmail.com>
2021-12-09 17:26:31 -0300

    Agrega filtros de subcategorias y buscador

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -26,6 +26,33 @@ let controller = {
             category,
             subcategories: uniqueSubcategories
         })
+    },
+    subcategory: (req, res) => {
+       let subcategoryName = req.params.subcategory.toLowerCase()
+       let categoryId = +req.params.categoryId 
+       
+       let productsSubcategories = products.filter(product => product.subcategory.toLowerCase() === subcategoryName)
+       let productsCategory = products.filter(product => +product.category === categoryId)
+       let category = categories.find(category => category.id === categoryId)
+       let subcategories = productsCategory.map(product => product.subcategory)
+       let uniqueSubcategories = subcategories.filter((x, i, a) => a.indexOf(x) == i)
+
+       res.render('subcategory', {
+           products: productsSubcategories,
+           category,
+           subcategories: uniqueSubcategories
+       })
+    },
+    search: (req, res) => {
+        let keywords = req.query.keywords.trim().toLowerCase()
+
+        let result = products.filter(product => product.name.toLowerCase().includes(keywords))
+        
+        res.render('searchResult', {
+            result,
+            search: keywords
+        })
+
     }
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -8,4 +8,10 @@ router.get('/detail/:id', controller.detail)
 /* GET - List of products */
 router.get('/category/:id', controller.category)
 
+/* GET - List of product (Subcategories) */
+router.get('/subcategory/:subcategory/:categoryId', controller.subcategory)
+
+/* GET - Search products */
+router.get('/search', controller.search)
+
 module.exports = router
